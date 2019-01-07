4f383ef1f0bd5b7ca4fd4667a083fa8466e14e0a
许瀚文 <842644519@qq.com>
2019-01-07 18:53:52 +0800

    reducer

--- react-lesson/src/redux.js
+++ react-lesson/src/redux.js
@@ -1,5 +1,22 @@
 let createStore = (reducer) => {
-
+    let state = {}
+    let listener = []
+    let getState = () => state
+    let dispath = (action) => {
+        state = reducer(state, action)
+        listener.forEach(fn => fn())
+    }
+    let subscribe = (fn) => {
+        listener.push(fn)
+        return () => {
+            listener = listener.filter(e => e !== fn)
+        }
+    }
+    return {
+        getState,
+        dispath,
+        subscribe
+    }
 }
 
 export default {
